ef11efbe22799e81e87d5cf32ad2d1e15df89330
Blake Sutton <sutton.blake@gmail.com>
2015-12-03 20:41:48 -0700

    added in controller test case

---
+++ tests/TestCase/Controller/PluginSystemPluginsControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace PluginSystem\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use PluginSystem\Controller\PluginSystemPluginsController;
+
+/**
+ * PluginSystem\Controller\PluginSystemPluginsController Test Case
+ */
+class PluginSystemPluginsControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.plugin_system.plugin_system_plugins'
+    ];
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test activate method
+     *
+     * @return void
+     */
+    public function testActivate()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test deactivate method
+     *
+     * @return void
+     */
+    public function testDeactivate()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
