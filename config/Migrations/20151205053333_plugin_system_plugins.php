6c07b362c1b432f2801405be84dffdf4c469bfbc
Blake Sutton <sutton.blake@gmail.com>
2015-12-04 23:01:12 -0700

    Update 20151205053333_plugin_system_plugins.php

--- config/Migrations/20151205053333_plugin_system_plugins.php
+++ config/Migrations/20151205053333_plugin_system_plugins.php
@@ -4,13 +4,71 @@ use Migrations\AbstractMigration;
 class PluginSystemPlugins extends AbstractMigration
 {
     /**
-     * Change Method.
+     * Up Method.
      *
      * More information on this method is available here:
      * http://docs.phinx.org/en/latest/migrations.html#the-change-method
      * @return void
      */
-    public function change()
+    public function up()
     {
+                $table = $this->table('plugin_system_plugins');
+        $table
+            ->addColumn('system_name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('uri', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('author', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('version', 'string', [
+                'default' => null,
+                'limit' => 30,
+                'null' => false,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'name',
+                ],
+                ['unique' => true]
+            )
+            ->create();
+
+        $table = $this->table('roles');
+        $table
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false,
+            ])
+            ->create();
     }
 }
